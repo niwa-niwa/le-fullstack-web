e4b3afdfd713c71902f2504b1e42c0fcb36c9293
niwa-niwa <urban.hang274@gmail.com>
2021-10-09 12:52:52 +0900

    implemented a function for editing post

--- client/pages/post/[id].js
+++ client/pages/post/[id].js
@@ -78,13 +78,66 @@ export default function Post() {
         router.push("/");
       });
   };
+
+  const editPost = (option) => {
+    if (option === "title") {
+      let newTitle = prompt("Enter New Title:");
+      axios
+        .put(
+          "http://localhost:3001/posts/title",
+          { newTitle, id },
+          {
+            headers: {
+              accessToken: localStorage.getItem("accessToken"),
+            },
+          }
+        )
+        .then((response) => {
+          setPostObject({ ...postObject, title: newTitle });
+        });
+    } else {
+      let newText = prompt("Enter New Text:");
+      axios
+        .put(
+          "http://localhost:3001/posts/postText",
+          { newText, id },
+          {
+            headers: {
+              accessToken: localStorage.getItem("accessToken"),
+            },
+          }
+        )
+        .then((response) => {
+          setPostObject({ ...postObject, postText: newText });
+        });
+    }
+  };
+
   return (
     <Layout>
       <div className="postPage">
         <div className="leftSide">
           <div className="post" id="individual">
-            <div className="title">{postObject.title}</div>
-            <div className="body">{postObject.postText}</div>
+            <div
+              className="title"
+              onClick={() => {
+                if (authState.username === postObject.username) {
+                  editPost("title");
+                }
+              }}
+            >
+              {postObject.title}
+            </div>
+            <div
+              className="body"
+              onClick={() => {
+                if (authState.username === postObject.username) {
+                  editPost("body");
+                }
+              }}
+            >
+              {postObject.postText}
+            </div>
             <div className="footer">
               {postObject.username}
               {authState.username === postObject.username && (
